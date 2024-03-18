cc186ee226e6d9c3aa2f15aa22822392ea2930ff
Gabor <kapusgabor64@gmail.com>
2024-03-18 13:38:33 +0100

    modify user's table name regarding h2 test config

--- src/main/java/hu/webler/weblerapartmentreservation/apartment/entity/User.java
+++ src/main/java/hu/webler/weblerapartmentreservation/apartment/entity/User.java
@@ -1,9 +1,6 @@
 package hu.webler.weblerapartmentreservation.apartment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "reservation_user")
 public class User {
 
     @Id
