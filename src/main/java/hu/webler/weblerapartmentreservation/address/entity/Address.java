78d2cf40dfac16c379f070b65d70c18cc4db417d
Peter Futo <futopeter97@gmail.com>
2024-03-25 12:51:35 +0100

    Create Address entity

---
+++ src/main/java/hu/webler/weblerapartmentreservation/address/entity/Address.java
@@ -0,0 +1,27 @@
+package hu.webler.weblerapartmentreservation.address.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String country;
+    private String postalCode;
+    private String city;
+    private String addressLine;
+}
