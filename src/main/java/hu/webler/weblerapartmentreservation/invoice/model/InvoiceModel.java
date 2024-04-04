7bcd7a0e4b8c7d44786b7c97b51f5953de4cac4a
Peter Futo <futopeter97@gmail.com>
2024-04-04 10:08:31 +0200

    Remove unused imports

--- src/main/java/hu/webler/weblerapartmentreservation/invoice/model/InvoiceModel.java
+++ src/main/java/hu/webler/weblerapartmentreservation/invoice/model/InvoiceModel.java
@@ -1,10 +1,8 @@
 package hu.webler.weblerapartmentreservation.invoice.model;
 
 import hu.webler.weblerapartmentreservation.address.entity.Address;
-import hu.webler.weblerapartmentreservation.apartment.entity.Apartment;
 import hu.webler.weblerapartmentreservation.invoice.value.PaymentDate;
 import hu.webler.weblerapartmentreservation.invoice.value.PaymentMethod;
-import hu.webler.weblerapartmentreservation.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
