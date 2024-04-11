db6b7537f67e9975a3c44e8b15a8e5a7f3dc77bc
Szőke Roland <szokeroland0@gmail.com>
2024-04-11 14:45:25 +0200

    Modified createUser method

--- src/main/java/hu/webler/weblerapartmentreservation/user/controller/UserController.java
+++ src/main/java/hu/webler/weblerapartmentreservation/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package hu.webler.weblerapartmentreservation.user.controller;
 
+import hu.webler.weblerapartmentreservation.address.entity.Address;
 import hu.webler.weblerapartmentreservation.user.entity.User;
 import hu.webler.weblerapartmentreservation.user.model.UserCreateModel;
 import hu.webler.weblerapartmentreservation.user.model.UserModel;
@@ -28,9 +29,9 @@ public class UserController {
         return ResponseEntity.status(200).body(userService.findUserById(id));
     }
 
-    @PostMapping("/users")
-    public ResponseEntity<UserModel> createUser(@RequestBody UserCreateModel userCreateModel) {
-        return ResponseEntity.status(200).body(userService.createUser(userCreateModel));
+    @PostMapping("/users/address/{addressId}")
+    public ResponseEntity<UserModel> createUser(@RequestBody UserCreateModel userCreateModel,@PathVariable(value = "addressId") Long addressId) {
+        return ResponseEntity.status(200).body(userService.createUser(userCreateModel, addressId));
     }
 
     @DeleteMapping("/users/{id}")
