42432e3d3a6d4eb1fb8f23cfc55732748603a971
Szőke Roland <szokeroland0@gmail.com>
2024-04-14 09:28:00 +0200

    Modified logic for creating apartment, implemented Address to createApartment method

--- src/main/java/hu/webler/weblerapartmentreservation/apartment/service/ApartmentService.java
+++ src/main/java/hu/webler/weblerapartmentreservation/apartment/service/ApartmentService.java
@@ -1,5 +1,8 @@
 package hu.webler.weblerapartmentreservation.apartment.service;
 
+import hu.webler.weblerapartmentreservation.address.entity.Address;
+import hu.webler.weblerapartmentreservation.address.model.AddressCreateModel;
+import hu.webler.weblerapartmentreservation.address.persistence.AddressRepository;
 import hu.webler.weblerapartmentreservation.address.service.AddressService;
 import hu.webler.weblerapartmentreservation.apartment.entity.Apartment;
 import hu.webler.weblerapartmentreservation.apartment.model.ApartmentCreateModel;
@@ -12,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,6 +26,7 @@ public class ApartmentService {
 
     private final ApartmentRepository apartmentRepository;
     private final AddressService addressService;
+    private final AddressRepository addressRepository;
 
     public List<ApartmentModel> findAllApartments() {
         return apartmentRepository.findAll()
@@ -34,14 +40,48 @@ public class ApartmentService {
                 .orElseThrow( () -> {
                     String message = String.format("Apartment with id %d was not found", id);
                     log.info(message);
-                    return new RuntimeException(message);
+                    return new NoSuchElementException(message);
                         });
     }
 
     public ApartmentModel createApartment(ApartmentCreateModel apartmentCreateModel) {
-        // TODO logic, if address is in database set that, if not, save first a new address, than save!
-        // similar logic as in User ...
+        Address addressToSave = null;
+        if (apartmentCreateModel.getAddress() != null) {
+            Optional<Address> optionalAddress = addressRepository.findAddressByCountryAndPostalCodeAndCityAndLine(
+                    apartmentCreateModel.getAddress().getCountry(),
+                    apartmentCreateModel.getAddress().getPostalCode(),
+                    apartmentCreateModel.getAddress().getCity(),
+                    apartmentCreateModel.getAddress().getLine());
+
+            if (optionalAddress.isPresent()) {
+                addressToSave = optionalAddress.get();
+            } else {
+                AddressCreateModel model = new AddressCreateModel();
+                model.setCountry(apartmentCreateModel.getAddress().getCountry());
+                model.setPostalCode(apartmentCreateModel.getAddress().getPostalCode());
+                model.setCity(apartmentCreateModel.getAddress().getCity());
+                model.setLine(apartmentCreateModel.getAddress().getLine());
+                addressToSave = createAddress(model);
+            }
+        } else {
+            throw new IllegalArgumentException("Address details are required");
+        }
+        apartmentCreateModel.setAddress(addressToSave);
         return ApartmentMapper.mapApartmentEntityToApartmentModel(apartmentRepository.save(ApartmentMapper.mapApartmentCreateModelToApartmentEntity(apartmentCreateModel)));
+    }
+
+    private Address createAddress(AddressCreateModel addressCreateModel) {
+        Address address = new Address();
+        address.setCountry(addressCreateModel.getCountry());
+        address.setPostalCode(addressCreateModel.getPostalCode());
+        address.setCity(addressCreateModel.getCity());
+
+        if (addressCreateModel.getLine() != null) {
+            address.setLine(addressCreateModel.getLine());
+        } else {
+            address.setLine("");
+        }
+        return addressRepository.save(address);
     }
 
     public void deleteApartment(Long id) {
