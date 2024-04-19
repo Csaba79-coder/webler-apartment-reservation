146e37dab31015609a3ed9086f2c30f4dc86f7cd
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-19 15:26:27 +0200

    create navbar.html regarding DRY principles update html files, and create controller for all navbar element

---
+++ src/main/java/hu/webler/weblerapartmentreservation/view/AboutUsWebController.java
@@ -0,0 +1,15 @@
+package hu.webler.weblerapartmentreservation.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thy")
+public class AboutUsWebController {
+
+    @GetMapping("/about-us")
+    public String homePage() {
+        return "about-us";
+    }
+}
