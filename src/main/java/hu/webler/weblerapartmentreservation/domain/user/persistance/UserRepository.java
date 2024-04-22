9352072da760b8e62de0878012970dd21d5b57d3
Szőke Roland <szokeroland0@gmail.com>
2024-04-22 10:47:20 +0200

    added Optional<User> findUserByFirstNameAndLastNameAndEmailAndPhoneNumber(String firstName, String lastName, String email,String phoneNumber)

--- src/main/java/hu/webler/weblerapartmentreservation/domain/user/persistance/UserRepository.java
+++ src/main/java/hu/webler/weblerapartmentreservation/domain/user/persistance/UserRepository.java
@@ -1,9 +1,15 @@
 package hu.webler.weblerapartmentreservation.domain.user.persistance;
 
+import hu.webler.weblerapartmentreservation.domain.address.entity.Address;
 import hu.webler.weblerapartmentreservation.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByFirstNameAndLastNameAndEmailAndPhoneNumber(String firstName, String lastName, String email,
+                                                                                   String phoneNumber);
 }
