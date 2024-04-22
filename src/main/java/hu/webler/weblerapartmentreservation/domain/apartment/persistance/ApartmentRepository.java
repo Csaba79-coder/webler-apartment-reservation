051dca770f0467222787216bc487c2f2dca218ea
Szőke Roland <szokeroland0@gmail.com>
2024-04-22 10:49:46 +0200

    added Optional<Apartment> findApartmentByFloorNumberAndRoomNumberAndMinGuestAndMaxGuestAndApartmentTypeAndDescriptionAndApartmentStatusAndPrice(                   Integer floorNumber, Integer roomNumber, Integer minGuest, Integer maxGuest, ApartmentType apartmentType,                   String description, ApartmentStatus apartmentStatus, BigDecimal price)

--- src/main/java/hu/webler/weblerapartmentreservation/domain/apartment/persistance/ApartmentRepository.java
+++ src/main/java/hu/webler/weblerapartmentreservation/domain/apartment/persistance/ApartmentRepository.java
@@ -1,9 +1,18 @@
 package hu.webler.weblerapartmentreservation.domain.apartment.persistance;
 
 import hu.webler.weblerapartmentreservation.domain.apartment.entity.Apartment;
+import hu.webler.weblerapartmentreservation.domain.apartment.value.ApartmentStatus;
+import hu.webler.weblerapartmentreservation.domain.apartment.value.ApartmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 @Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
+
+    Optional<Apartment> findApartmentByFloorNumberAndRoomNumberAndMinGuestAndMaxGuestAndApartmentTypeAndDescriptionAndApartmentStatusAndPrice(
+            Integer floorNumber, Integer roomNumber, Integer minGuest, Integer maxGuest, ApartmentType apartmentType,
+            String description, ApartmentStatus apartmentStatus, BigDecimal price);
 }
