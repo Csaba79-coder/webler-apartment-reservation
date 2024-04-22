c0333997d7e4a5aa85117af7e59e2ecf0a09a96b
Szőke Roland <szokeroland0@gmail.com>
2024-04-22 10:46:44 +0200

    added Optional<Invoice> findInvoiceByGenerationDateAndPaymentTypeAndPaymentDate(LocalDateTime generationDate, PaymentType paymentType, LocalDate paymentDate)

--- src/main/java/hu/webler/weblerapartmentreservation/domain/invoice/persistence/InvoiceRepository.java
+++ src/main/java/hu/webler/weblerapartmentreservation/domain/invoice/persistence/InvoiceRepository.java
@@ -1,10 +1,17 @@
 package hu.webler.weblerapartmentreservation.domain.invoice.persistence;
 
 import hu.webler.weblerapartmentreservation.domain.invoice.entity.Invoice;
+import hu.webler.weblerapartmentreservation.domain.invoice.value.PaymentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
+    Optional<Invoice> findInvoiceByGenerationDateAndPaymentTypeAndPaymentDate(LocalDateTime generationDate,
+                                                                              PaymentType paymentType, LocalDate paymentDate);
 }
