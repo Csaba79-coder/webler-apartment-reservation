39e511bb8335aeabad1b545cca52199d064ee2a4
Peter Futo <futopeter97@gmail.com>
2024-04-23 17:47:43 +0200

    InvoiceServiceTest test for findById method

--- src/test/java/hu/webler/weblerapartmentreservation/domain/invoice/service/InvoiceServiceTest.java
+++ src/test/java/hu/webler/weblerapartmentreservation/domain/invoice/service/InvoiceServiceTest.java
@@ -14,11 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -65,5 +64,48 @@ public class InvoiceServiceTest {
         assertThat(invoices)
                 .usingRecursiveComparison()
                 .isEqualTo(invoiceData);
+    }
+
+    @Test
+    @DisplayName("Given valid invoice id when findInvoiceById() then returns invoice entity")
+    public void givenValidInvoiceId_whenFindInvoiceById_thenReturnsInvoiceEntity() {
+        // Given
+        Long random = new Random().nextLong(1, 4);
+
+        List<Invoice> invoiceData = List.of(
+                new Invoice(1L, LocalDateTime.now(), PaymentType.CARD,
+                        LocalDate.now(), new Address(), new ArrayList<>()),
+                new Invoice(2L, LocalDateTime.now(), PaymentType.CARD,
+                        LocalDate.now(), new Address(), new ArrayList<>()),
+                new Invoice(3L, LocalDateTime.now(), PaymentType.CARD,
+                        LocalDate.now(), new Address(), new ArrayList<>()),
+                new Invoice(4L, LocalDateTime.now(), PaymentType.CARD,
+                        LocalDate.now(), new Address(), new ArrayList<>())
+        );
+
+        Invoice invoice = invoiceData.get(random.intValue() - 1);
+        Long searchId = invoice.getId();
+
+        when(invoiceRepository.findById(random)).thenReturn(Optional.ofNullable(invoice));
+
+        // When
+        Invoice invoiceResult = invoiceService.findInvoiceById(searchId);
+
+        // Then
+        assertThat(invoiceResult).isNotNull();
+        assertThat(invoiceResult)
+                .isEqualTo(invoice);
+    }
+
+    @Test
+    @DisplayName("Given invalid invoice id when findInvoiceById() then throws new NoSuchElementException")
+    public void givenInvalidInvoiceId_whenFindInvoiceById_thenThrowsNoSuchElementException() {
+        // Given
+        Long searchId = new Random().nextLong(1, 100);
+
+        // When / Then
+        assertThatThrownBy(() -> invoiceService.findInvoiceById(searchId))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("Invoice with id " + searchId + " was not found");
     }
 }
