37351d22b535713a8c5354c3ac6187d9aebb0ce1
Peter Futo <futopeter97@gmail.com>
2024-04-23 16:40:59 +0200

    Remove contextLoads() test

--- src/test/java/hu/webler/weblerapartmentreservation/WeblerApartmentReservationApplicationTests.java
+++ src/test/java/hu/webler/weblerapartmentreservation/WeblerApartmentReservationApplicationTests.java
@@ -5,9 +5,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeblerApartmentReservationApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
