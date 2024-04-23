f278910b52f0108cd39e8ffd763e27c9011f01cd
Szőke Roland <szokeroland0@gmail.com>
2024-04-23 06:43:01 +0200

    removed unused field

--- src/main/java/hu/webler/weblerapartmentreservation/domain/apartment/model/ApartmentCreateModel.java
+++ src/main/java/hu/webler/weblerapartmentreservation/domain/apartment/model/ApartmentCreateModel.java
@@ -1,7 +1,6 @@
 package hu.webler.weblerapartmentreservation.domain.apartment.model;
 
 import hu.webler.weblerapartmentreservation.domain.address.entity.Address;
-import hu.webler.weblerapartmentreservation.domain.apartment.value.ApartmentStatus;
 import hu.webler.weblerapartmentreservation.domain.apartment.value.ApartmentType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,7 +20,6 @@ public class ApartmentCreateModel {
     private Integer minGuest;
     private Integer maxGuest;
     private ApartmentType apartmentType;
-    private ApartmentStatus apartmentStatus;
     private String description;
     private BigDecimal price;
     private Address address;
