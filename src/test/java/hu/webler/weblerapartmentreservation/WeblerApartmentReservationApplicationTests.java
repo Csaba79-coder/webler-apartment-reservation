40c3ba0600b1fa7de45c2d25872974bb58b56927
Gabor <kapusgabor64@gmail.com>
2024-04-25 11:48:27 +0200

    update UserServiceTest with CreateUser and Delete

--- src/test/java/hu/webler/weblerapartmentreservation/WeblerApartmentReservationApplicationTests.java
+++ src/test/java/hu/webler/weblerapartmentreservation/WeblerApartmentReservationApplicationTests.java
@@ -1,13 +1,7 @@
 package hu.webler.weblerapartmentreservation;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeblerApartmentReservationApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
--- src/test/java/hu/webler/weblerapartmentreservation/domain/user/service/UserServiceTest.java
+++ src/test/java/hu/webler/weblerapartmentreservation/domain/user/service/UserServiceTest.java
@@ -3,8 +3,10 @@ package hu.webler.weblerapartmentreservation.domain.user.service;
 import hu.webler.weblerapartmentreservation.domain.address.entity.Address;
 import hu.webler.weblerapartmentreservation.domain.address.persistence.AddressRepository;
 import hu.webler.weblerapartmentreservation.domain.user.entity.User;
+import hu.webler.weblerapartmentreservation.domain.user.model.UserCreateModel;
 import hu.webler.weblerapartmentreservation.domain.user.model.UserModel;
 import hu.webler.weblerapartmentreservation.domain.user.persistance.UserRepository;
+import hu.webler.weblerapartmentreservation.domain.user.util.UserMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +18,9 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -87,7 +92,7 @@ public class UserServiceTest {
         User user = userData.get(random.intValue() - 1);
         Long searchId = user.getId();
 
-        when(userRepository.findById(random)).thenReturn(Optional.ofNullable(user));
+        when(userRepository.findById(random)).thenReturn(Optional.of(user));
 
         // When
         User userResult = userService.findUserById(searchId);
@@ -108,5 +113,93 @@ public class UserServiceTest {
         assertThatThrownBy(() -> userService.findUserById(searchId))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessage("User with id " + searchId + " was not found");
+    }
+
+    @Test
+    @DisplayName("Given valid userCreateModel when creating user then returns user model")
+    public void givenValidUserCreateModel_whenCreatingUser_thenReturnsUserModel() {
+        // Given
+        Address address = new Address(1L, "Test data", "Test data", "Test data", "Test data");
+
+        UserCreateModel userCreateModel = new UserCreateModel();
+        userCreateModel.setFirstName("First Name");
+        userCreateModel.setLastName("Last Name");
+        userCreateModel.setEmail("Email");
+        userCreateModel.setPhoneNumber("PhoneNumber");
+        userCreateModel.setAddress(address);
+
+        // Mock
+        UserModel expectedModel = new UserModel();
+        expectedModel.setId(1L);
+        expectedModel.setFirstName("First Name");
+        expectedModel.setLastName("Last Name");
+        expectedModel.setEmail("Email");
+        expectedModel.setPhoneNumber("PhoneNumber");
+        expectedModel.setAddress(address);
+
+        when(userRepository.save(any())).thenReturn(UserMapper.mapUserCreateModelToUserEntity(userCreateModel));
+
+        // When
+        UserModel createdUserModel = userService.createUser(userCreateModel);
+
+        // Then
+        then(expectedModel)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(createdUserModel);
+    }
+
+    @Test
+    @DisplayName("Given missing address info when creating user then throws IllegalArgumentException")
+    public void givenMissingAddress_whenCreatingApartment_thenThrowsIllegalArgumentException() {
+        // Given
+
+        UserCreateModel userCreateModel = new UserCreateModel();
+        userCreateModel.setFirstName("First Name");
+        userCreateModel.setLastName("Last Name");
+        userCreateModel.setEmail("Email");
+        userCreateModel.setPhoneNumber("PhoneNumber");
+
+        // When / Then
+        assertThatThrownBy(() -> userService.createUser(userCreateModel))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Address details are required");
+    }
+
+    @Test
+    @DisplayName("Given missing user info when creating user then throws NullPointerException")
+    public void givenMissingUserInfo_whenCreatingUser_thenThrowsNullPointerException() {
+        // Given
+        Address address = new Address();
+
+        UserCreateModel userCreateModel = new UserCreateModel();
+        userCreateModel.setFirstName("First Name");
+        userCreateModel.setEmail("Email");
+        userCreateModel.setPhoneNumber("PhoneNumber");
+        userCreateModel.setAddress(address);
+
+        // When / Then
+        assertThatThrownBy(() -> userService.createUser(userCreateModel))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("Given valid user id when deleting user then verify")
+    public void givenValidUserId_whenDeletingAddress_thenVerify() {
+        // Given
+        Long id = new Random().nextLong();
+        User user = UserMapper.mapUserCreateModelToUserEntity(
+                new UserCreateModel("First Name", "Last Name", "Email", "PhoneNumber", new Address()));
+
+        user.setId(id);
+        userRepository.save(user);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+
+        // When
+        userService.deleteUser(id);
+
+        // Then#
+        verify(userRepository).deleteById(any());
     }
 }
